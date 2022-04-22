0f2fc108cde841b99f98333b611cd81e95be1516
Kenneth-Raphael Keil <s79578@smb.informatik.htw-dresden.de>
2022-04-22 14:52:12 +0200

    update

--- de.htwdd.edtma.jdbc/src/pka/excercise/solution/JDBCConnection.java
+++ de.htwdd.edtma.jdbc/src/pka/excercise/solution/JDBCConnection.java
@@ -3,23 +3,47 @@ import java.sql.*;
 
 public class JDBCConnection {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws SQLException {
 
-        String databaseURL = "jdbc:ucanaccess://Users/kenneth-raphaelkeil/Desktop/Studium/Semester 6/PKA/Turnierverwaltung.accdb";
+    	// no driver registration, driver is found in "lib"
+        String databaseURL = "jdbc:ucanaccess://U:/tmp/Turnierverwaltung.accdb";
+        
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet result = null;
+        
+        try {
 
-        try (Connection connection = DriverManager.getConnection(databaseURL)) {
+        	String sql = "SELECT s.Vereinsname, a.Strasse, a.Hausnummer, a.PLZ, a.Ort FROM Schachverein s JOIN Adresse a ON s.adresse = a.ID ORDER BY Vereinsname ASC";
+        	
+        	// 1. get a Connection
+        	connection = DriverManager.getConnection(databaseURL);
+        	
+        	// 2. create a SQL-Statement
+            statement = connection.createStatement();
+            
+            // 3. execute Query
+            result = statement.executeQuery(sql);
 
-            String sql = "SELECT * FROM Verein";
-
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(sql);
-
+            // 4. process the result
             while (result.next()) {
-                System.out.println(result.getString(1));
+            	/*
+                System.out.println(result.getString("Vereinsname") + ", " 
+                	+ result.getString("Strasse") + ", " 
+                	+ result.getString("Hausnummer") + ", "
+                	+ result.getString("PLZ") + ", "
+                	+ result.getString("Ort")
+                );
+                */
+            	// result.getInt()
+            	// result.getDate()
             }
-
         } catch (SQLException ex) {
-            ex.printStackTrace();
+        	ex.printStackTrace();
+        } finally {
+        	if (result != null) result.close();
+        	if (statement != null) statement.close();
+        	if (connection != null) connection.close();
         }
     }
 }
